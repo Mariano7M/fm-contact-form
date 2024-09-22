e953e21e5f38989b7bd749afd0d709ff9e485e94
Mariano <marinhoted@gmail.com>
2024-09-21 20:24:31 -0600

    feat: updating app component to handle a contact us form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormFieldComponent } from './components/form-field/form-field.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormFieldRadioComponent } from './components/form-field-radio/form-field-radio.component';
+import { FormFieldRadioGroupComponent } from './components/form-field-radio-group/form-field-radio-group.component';
+import { FormFieldCheckboxComponent } from './components/form-field-checkbox/form-field-checkbox.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    RouterOutlet,
+    FormFieldComponent,
+    ReactiveFormsModule,
+    FormFieldRadioComponent,
+    FormFieldRadioComponent,
+    FormFieldRadioGroupComponent,
+    FormFieldCheckboxComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'fm-contact-form';
+  formBuilder: FormBuilder = inject(FormBuilder);
+  contactForm!: FormGroup;
+
+  constructor() {
+    this.initializeContactForm();
+  }
+
+  private initializeContactForm() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      queryType: ['', [Validators.required]],
+      message: ['', [Validators.required]],
+      termsAndConditions: [null, [Validators.requiredTrue]],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.contactForm.value);
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+  }
 }
